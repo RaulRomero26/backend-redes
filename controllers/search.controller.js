b906b2d34aff50216559a39abfee93e676dbd223
RaulRomero26 <link_2642@hotmail.com>
2024-12-19 13:02:40 -0600

    UPDATE: VALIDACION permiso

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -120,13 +120,13 @@ const VehiculoByInspeccion = async (req = request, res = response) => {
 
     if (inspeccion != undefined && inspeccion != '') {
         query += `vehiculo_inspeccion.Id_Inspeccion = ${inspeccion}`;
-    } else if (placas != undefined && placas != '' && !['SIN PLACA','sin placa','SD', 'S/D', 'sd', 's/d','sp','s/p','SP','S/P'].includes(placas) && placas.length>4) {
+    } else if (placas != undefined && placas != '' && !['PERMISO','permiso','SIN PLACA','sin placa','SD', 'S/D', 'sd', 's/d','sp','s/p','SP','S/P'].includes(placas) && placas.length>4) {
         query += `vehiculo_inspeccion.Placas_Vehiculo = '${placas}'`;
     } else if (niv != undefined && niv != '' && !['SIN NIV','sin niv','SD', 'S/D', 'sd', 's/d'].includes(niv)) {
         query += `vehiculo_inspeccion.NIV = '${niv}'`;
     } else if (placas != undefined && niv != undefined) {
         let conditions = [];
-        if (!['SIN PLACA','sin placa','SD', 'S/D', 'sd', 's/d','sp','s/p','SP','S/P'].includes(placas)) {
+        if (!['PERMISO','permiso','SIN PLACA','sin placa','SD', 'S/D', 'sd', 's/d','sp','s/p','SP','S/P'].includes(placas)) {
             conditions.push(`vehiculo_inspeccion.Placas_Vehiculo = '${placas}'`);
         }
         if (!['SIN NIV','sin niv','SD', 'S/D', 'sd', 's/d'].includes(niv)) {
@@ -320,7 +320,7 @@ const BuscarVehiculo = async (req = request, res = response) => {
     const { placa, niv } = req.body;
     let query = '';
 
-    if (placa != undefined && placa != 'SIN PLACA' && placa != 'sin placa' && placa != '' && placa != 'SD' && placa != 'S/D' && placa != 'sp' && placa != 'SP' && placa != 's/p' && placa != 'S/P' && placa.length>4) {
+    if (placa != undefined && placa != 'permiso' && placa != 'PERMISO' && placa != 'SIN PLACA' && placa != 'sin placa' && placa != '' && placa != 'SD' && placa != 'S/D' && placa != 'sp' && placa != 'SP' && placa != 's/p' && placa != 'S/P' && placa.length>4) {
         query = `
         SELECT vehiculos.*, argos_vehiculos_asegurados.*
         FROM argos_vehiculos_asegurados 
